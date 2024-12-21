31946dc643b33fe0f3d2ca5ddbe54d29bbb0942d
SputnikCap <148067873+SputnikCap@users.noreply.github.com>
2024-12-21 12:06:50 +0300

    Update context.ts

--- context.ts
+++ context.ts
@@ -1,7 +1,6 @@
 import { Scenes, Context as TelegrafContext } from 'telegraf';
 import { SessionData } from './SessionData';
 
-// Определение контекста бота
 export interface MyContext extends TelegrafContext {
     scene: Scenes.SceneContextScene<MyContext, SessionData>;
     wizard: Scenes.WizardContextWizard<MyContext>;
